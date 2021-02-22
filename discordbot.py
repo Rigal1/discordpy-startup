5baeb9454236f79db6d160742c4b032060eb9045
Rigal1 <76402544+Rigal1@users.noreply.github.com>
2021-02-22 19:41:04 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,10 +1,37 @@
-from discord.ext import commands
-import os
-import traceback
+import discord
+import diceSearchAndCalc as dice
+token = "ODExODgyMjE5NzM5MDIxMzEy.YC4qPQ.aAJcTTgYFFqmrEm4kJRRbm4HeH4"
+client = discord.Client()
 
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
+@client.event
+async def on_ready():
+    # 起動したらターミナルにログイン通知が表示される
+    #print('ログインしました')
 
+# メッセージ受信時に動作する処理
+@client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    #print(message.content[0:2])
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    
+    if client.user in message.mentions: # 話しかけられたかの判定
+        await reply(message)
+    
+    if message.content == '/neko':
+        await message.channel.send('にゃーん')
+    elif message.content[0:2] == "/r":
+        diceText = message.content[3:]
+        resultDice = dice.replaceAndCalc(diceText)
+        await reply(message, resultDice)
+        #await message.channel.send(resultDice)
+            
+    #print(message)
+async def reply(message, result):
+    reply = f'{message.author.mention} {result}' # 返信メッセージの作成
+    await message.channel.send(reply) # 返信メッセージを送信
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -12,10 +39,4 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-
-bot.run(token)
+client.run(token)
