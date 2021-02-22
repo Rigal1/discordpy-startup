ad039235ac2635f6911c0f8fafd942eb3e8bba4b
Rigal1 <76402544+Rigal1@users.noreply.github.com>
2021-02-22 20:49:50 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,7 +1,7 @@
 from discord.ext import commands
 import os
 import traceback
-import diceSearchAndCalc as dice
+#import diceSearchAndCalc as dice
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
@@ -13,10 +13,8 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
-
-    
 @bot.command()
-async def r(ctx):
+async def ping(ctx):
     await ctx.send('pong')
 
 
