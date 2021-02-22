4515ce716a6b79fcc4b3f69dc63a10490aae6074
Rigal1 <76402544+Rigal1@users.noreply.github.com>
2021-02-22 20:24:06 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 import os
 import traceback
+import diceSearchAndCalc as dice
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
@@ -12,10 +13,28 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
+@bot.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    #print(message.content[0:2])
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    
+    if bot.user in message.mentions: # 話しかけられたかの判定
+        await reply(message)
+    
+    if message.content == '/neko':
+        await message.channel.send('にゃーん')
+    elif message.content[0:2] == "/r":
+        diceText = message.content[3:]
+        resultDice = dice.replaceAndCalc(diceText)
+        await reply(message, resultDice)
+        #await message.channel.send(resultDice)
+            
+    #print(message)
+async def reply(message, result):
+    reply = f'{message.author.mention} {result}' # 返信メッセージの作成
+    await message.channel.send(reply) # 返信メッセージを送信
 
 bot.run(token)
